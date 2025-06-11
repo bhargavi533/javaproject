342c6a9e4b8e0a6a09e4e90c3ba508ecaf23ff8a
bhargavi533 <saddhalabhargavi@gmail.com>
2025-06-11 10:43:20 +0530

    Add files via upload

---
+++ ReverseArray.java
@@ -0,0 +1,34 @@
+package Bhargavi;
+
+import java.util.Scanner;
+
+public class ReverseArray {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        
+        for (int i = 0; i < n / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[n - 1 - i];
+            arr[n - 1 - i] = temp;
+        }
+
+        System.out.println("Reversed array:");
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+
+        scanner.close();
+    }
+}
