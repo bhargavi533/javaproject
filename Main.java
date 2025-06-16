336e957d5a5587d2795ce932272cf038015db118
bhargavi533 <saddhalabhargavi@gmail.com>
2025-06-16 10:35:36 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,22 +1,26 @@
-package xyz;
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 
-abstract class Abstractexample {
-    abstract void Atm();
-     public void Atm2() {
-        System.out.println("without abstract atm");
-    }
-}
 
-class B extends Abstractexample {
-    void Atm() {
-        System.out.println("with abstract atm");
-    }
-}
-
-public class Main {
-    public static void main(String[] args) {
-        Abstractexample b = new B(); 
-        b.Atm(); 
-        b.Atm2(); 
-    }
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
